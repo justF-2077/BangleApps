d379d825877638f637dd54cd0c8a38d2f2be8b60
Ishidres <17363426+Ishidres@users.noreply.github.com>
2024-03-25 15:32:38 +0100

    Add new better step counter app

---
+++ apps/betterstepcounter/boot.js
@@ -0,0 +1,15 @@
+let lastStepTime = null;
+
+Bangle.on('step', function(steps) {
+  let currentTime = Date.now();
+
+  if (lastStepTime) {
+    let timeDiff = currentTime - lastStepTime;
+
+    if (timeDiff >= 5000) {
+      Bangle.setStepCount(steps - 1);
+    }
+  }
+
+  lastStepTime = currentTime;
+});
