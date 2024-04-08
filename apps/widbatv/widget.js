8b319d8a6d3ade4fef19de37aa86bf87c9a75cd1
Ishidres <17363426+Ishidres@users.noreply.github.com>
2024-04-08 15:16:36 +0200

    Update battery widget color thresholds

--- apps/widbatv/widget.js
+++ apps/widbatv/widget.js
@@ -3,9 +3,9 @@ Bangle.on('charging',function(charging) {
   WIDGETS["batv"].draw();
 });
 setInterval(()=>WIDGETS["batv"].draw(), 60000);
-Bangle.on('lcdPower', function(on) {
-  if (on) WIDGETS["batv"].draw(); // refresh at power on
-});
+// Bangle.on('lcdPower', function(on) {
+//   if (on) WIDGETS["batv"].draw(); // refresh at power on
+// });
 WIDGETS["batv"]={area:"tr",width:14,draw:function() {
   var x = this.x, y = this.y;
   g.reset();
@@ -15,8 +15,8 @@ WIDGETS["batv"]={area:"tr",width:14,draw:function() {
     g.clearRect(x,y,x+14,y+23);
     g.setColor(g.theme.fg).fillRect(x+2,y+2,x+12,y+22).clearRect(x+4,y+4,x+10,y+20).fillRect(x+5,y+1,x+9,y+2);
     var battery = E.getBattery();
-    if (battery < 20) {g.setColor("#f00");}
-    else if (battery < 40) {g.setColor(g.theme.dark ? "#ff0" : "#f80");}
+    if (battery < 15) {g.setColor("#f00");}
+    else if (battery < 30) {g.setColor(g.theme.dark ? "#ff0" : "#f80");}
     else {g.setColor("#0f0");}
     g.fillRect(x+4,y+20-(E.getBattery()*16/100),x+10,y+20);
   }
