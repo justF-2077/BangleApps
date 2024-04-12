dd855d789dc02802be6e5b38c00af25404163343
Ishidres <17363426+Ishidres@users.noreply.github.com>
2024-04-13 00:45:25 +0200

    Also pull immediately on BLE connect

--- apps/owmweather/boot.js
+++ apps/owmweather/boot.js
@@ -11,23 +11,22 @@
     require('Storage').writeJSON("owmweather.json", settings);
   }
 
+  let pullWeather = function() {
+    if (!waiting && NRF.getSecurityStatus().connected){
+      waiting = true;
+      require("owmweather").pull(completion);
+    }
+  }
+
   if (settings.enabled) {    
     let weather = require("Storage").readJSON('weather.json') || {};
     if (weather && weather.weather && weather.weather.time) lastUpdate = weather.weather.time;
 
     if (!settings.updated || settings.updated + settings.refresh * 1000 * 60 < Date.now()){
-      setTimeout(() => {
-        if (!waiting){
-          waiting = true;
-          require("owmweather").pull(completion);
-        }
-      }, 5000);
+      setTimeout(pullWeather, 5000);
     }
-    setInterval(() => {
-      if (!waiting && NRF.getSecurityStatus().connected){
-        waiting = true;
-        require("owmweather").pull(completion);
-      }
-    }, settings.refresh * 1000 * 60);
+    setInterval(pullWeather, settings.refresh * 1000 * 60);
   }
+
+  NRF.on('connect', pullWeather);
 }
