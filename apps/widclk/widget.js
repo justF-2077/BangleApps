0bbafe73b3859ebd39cb4433326353f0551acf42
Ishidres <17363426+Ishidres@users.noreply.github.com>
2024-04-23 00:25:22 +0200

    Fix time formatting in widget.js

--- apps/widclk/widget.js
+++ apps/widclk/widget.js
@@ -8,7 +8,9 @@ WIDGETS["wdclk"]={area:"tl",width:Bangle.CLOCK?0:52/* g.stringWidth("00:00") */,
   }
   if (!this.width) return; // if not visible, return
 g.reset().setFontCustom(atob("AAAAAAAAAAIAAAQCAQAAAd0BgMBdwAAAAAAAdwAB0RiMRcAAAERiMRdwAcAQCAQdwAcERiMRBwAd0RiMRBwAAEAgEAdwAd0RiMRdwAcERiMRdwAFAAd0QiEQdwAdwRCIRBwAd0BgMBAAABwRCIRdwAd0RiMRAAAd0QiEQAAAAAAAAAA="), 32, atob("BgAAAAAAAAAAAAAAAAYCAAYGBgYGBgYGBgYCAAAAAAAABgYGBgYG"), 512+9).setFontAlign(0,0);
-  var time = require("locale").time(new Date(),1);
+  var time = require("locale").time(new Date(),1).split(":");
+  time[0] = time[0][0] === " " ? "0" + time[0][1] : time[0];
+  time = time[0] + ":" + time[1];
   g.clearRect(this.x, this.y, this.x+this.width-1, this.y+23).drawString(time, this.x+this.width/2, this.y+12); // 5 * 6*2 = 60
   // queue draw in one minute
   if (this.drawTimeout) clearTimeout(this.drawTimeout);
