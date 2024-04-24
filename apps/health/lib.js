4cd4f07fff727c144e205bb45381a49ba670da6d
Ishidres <17363426+Ishidres@users.noreply.github.com>
2024-04-24 23:35:48 +0200

    Update health lib.js to include temperature in health record

--- apps/health/lib.js
+++ apps/health/lib.js
@@ -1,4 +1,4 @@
-const DB_RECORD_LEN = 4;
+const DB_RECORD_LEN = 5;
 const DB_RECORDS_PER_HR = 6;
 const DB_RECORDS_PER_DAY = DB_RECORDS_PER_HR*24 + 1/*summary*/;
 //const DB_RECORDS_PER_MONTH = DB_RECORDS_PER_DAY*31;
@@ -24,12 +24,13 @@ exports.readAllRecords = function(d, cb) {
     for (var hr=0;hr<24;hr++) { // actually 25, see below
       for (var m=0;m<DB_RECORDS_PER_HR;m++) {
         var h = f.substr(idx, DB_RECORD_LEN);
-        if (h!="\xFF\xFF\xFF\xFF") {
+        if (h!="\xFF\xFF\xFF\xFF\xFF") {
           cb({
             day:day+1, hr : hr, min:m*10,
             steps : (h.charCodeAt(0)<<8) | h.charCodeAt(1),
             bpm : h.charCodeAt(2),
-            movement : h.charCodeAt(3)*8
+            movement : h.charCodeAt(3)*8,
+            temperature : h.charCodeAt(4)
           });
         }
         idx += DB_RECORD_LEN;
@@ -77,12 +78,13 @@ exports.readDailySummaries = function(d, cb) {
   var idx = DB_HEADER_LEN + (DB_RECORDS_PER_DAY-1)*DB_RECORD_LEN; // summary is at the end of each day
   for (var day=0;day<31;day++) {
     var h = f.substr(idx, DB_RECORD_LEN);
-    if (h!="\xFF\xFF\xFF\xFF") {
+    if (h!="\xFF\xFF\xFF\xFF\xFF") {
       cb({
         day:day+1,
         steps : (h.charCodeAt(0)<<8) | h.charCodeAt(1),
         bpm : h.charCodeAt(2),
-        movement : h.charCodeAt(3)*8
+        movement : h.charCodeAt(3)*8,
+        temperature : h.charCodeAt(4)
       });
     }
     idx += DB_RECORDS_PER_DAY*DB_RECORD_LEN;
@@ -99,12 +101,13 @@ exports.readDay = function(d, cb) {
   for (var hr=0;hr<24;hr++) {
     for (var m=0;m<DB_RECORDS_PER_HR;m++) {
       var h = f.substr(idx, DB_RECORD_LEN);
-      if (h!="\xFF\xFF\xFF\xFF") {
+      if (h!="\xFF\xFF\xFF\xFF\xFF") {
         cb({
           hr : hr, min:m*10,
           steps : (h.charCodeAt(0)<<8) | h.charCodeAt(1),
           bpm : h.charCodeAt(2),
-          movement : h.charCodeAt(3)*8
+          movement : h.charCodeAt(3)*8,
+          temperature : h.charCodeAt(4)
         });
       }
       idx += DB_RECORD_LEN;
