66a449d015137a192d971d360e7f91aaa0170e67
F <17363426+justF-2077@users.noreply.github.com>
2025-06-10 20:20:59 +0200

    Update Bluetooth widget color logic based on connection status

--- apps/widbthide/widget.js
+++ apps/widbthide/widget.js
@@ -2,7 +2,7 @@ WIDGETS["bluetooth"]={area:"tr",draw:function() {
   if (WIDGETS.bluetooth.width==0)
     return;
   g.reset();
-  g.setColor((g.getBPP()>8) ? "#07f" : (g.theme.dark ? "#0ff" : "#00f"));
+  g.setColor((g.getBPP()>8) ? "#07f" : (NRF.getSecurityStatus().connected ? (g.theme.dark ? "#0ff" : "#00f") : "#f00"));
   g.drawImage(atob("CxQBBgDgFgJgR4jZMawfAcA4D4NYybEYIwTAsBwDAA=="),2+this.x,2+this.y);
 },changed:function() {
   WIDGETS.bluetooth.width = (NRF.getSecurityStatus().advertising || NRF.getSecurityStatus().connected || NRF.getSecurityStatus().bonded !== undefined)?15:0;
