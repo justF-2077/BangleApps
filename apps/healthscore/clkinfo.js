9640a24684474ebdd18dadcf1f44e4cc28567cc9
F <17363426+justF-2077@users.noreply.github.com>
2025-06-26 14:33:31 +0200

    Remove lib.js

--- apps/healthscore/clkinfo.js
+++ apps/healthscore/clkinfo.js
@@ -6,9 +6,34 @@
     saveStepCounts: true, // Whether to save all step counts above the count threshold
   }, require('Storage').readJSON("healthscore.json", true) || {});
 
+  function hsGetData(hs_data, settings) {
+    var total = { a: 0, i: 0 };
+    for (var i = 0; i < 7; i++) {
+      var date = new Date();
+      date.setDate(date.getDate() - i);
+      var dateString = date.toISOString().slice(0, 10);
+
+      if (hs_data[dateString]) {
+        for (var j = 0; j < hs_data[dateString].t.length; j++) {
+          var steps = hs_data[dateString].t[j];
+          if (steps >= settings.activeThreshold) {
+            total.a += 1; // Count this minute as active
+          }
+          if (steps >= settings.intenseThreshold) {
+            total.i += 1; // Count this minute as intense
+          }
+        }
+        // Add the minutes from the active and intense counts
+        total.a += hs_data[dateString].a;
+        total.i += hs_data[dateString].i;
+      }
+    }
+    return total;
+  }
+
   // Read health score data from storage
   var hs_data = require("Storage").readJSON("hs_data.json");
-  hs_data = require("healthscore").getData(hs_data, settings);
+  hs_data = hsGetData(hs_data, settings);
 
   // Calculate the health score
   var hsScore = ((hs_data.a + hs_data.i * 2) / 150) * 100;
